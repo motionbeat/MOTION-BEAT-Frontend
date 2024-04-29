1a85f17640f66942419920bcd7e094c0313fab36
AnyTHIW <79294133+AnyTHIW@users.noreply.github.com>
2024-04-29 23:51:38 +0900

    chore: add comment
    App.js 페이지 흐름 구분 및 주석 추가, forgot 컴포넌트 이름 변경, 소켓통신 추가, main.js 라우트 컴포넌트 중복 삭제

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
+/* 웹 접속 */
 import Title from "./components/title";
 import Splash from "./components/splash";
 
+/* 로그인 전 (인증인가 X)*/
 import Login from "./pages/login";
-import Forgot from "./pages/forgot";
+import ForgotPw from "./pages/forgotPw";
 import Signup from "./pages/signup";
+
+/* 메인 (인증인가 O)*/
 import Main from "./pages/main";
 import Mypage from "./pages/mypage";
 import Playtype from "./pages/playtype";
 import Tutorial from "./pages/tutorial";
 import Ranking from "./pages/ranking";
 import Setting from "./pages/setting";
+
 import Room from "./pages/room";
+
 import NotFound from "./pages/notFound";
 
 const App = () => {
+
   return (
-    <Router>
-      <ShowTitle />
-      <Routes>
-        {/* <Route path="/" element={<Navigate replace to="/login" />} /> */}
-        <Route path="/" element={<Splash />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot" element={<Forgot />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/playtype" element={<Playtype />} />
-        <Route path="/tutorial" element={<Tutorial />} />
-        <Route path="/ranking" element={<Ranking />} />
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/room" element={<Room />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <ShowTitle />
+        <Routes>
+          {/* <Route path="/" element={<Navigate replace to="/login" />} /> */}
+          {/* 웹 접속 */}
+          <Route path="/" element={<Splash />} />
+
+          {/* 로그인 전 */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgotPw" element={<ForgotPw />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* 메인 */}
+          <Route path="/main" element={<Main />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/playtype" element={<Playtype />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/setting" element={<Setting />} />
+
+          <Route path="/room" element={<Room />} />
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/pages/forgotPw.js
+++ src/pages/forgotPw.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { CheckForgotValidate } from "../utils/checkValidate";
 
-export default function Forgot() {
+export default function ForgotPw() {
   const [errors, setErrors] = useState({});
   const emailRef = useRef(null);
 
