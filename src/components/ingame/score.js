f91974582f895523df61b59bd0cb9a3d2f03b6b8
AnyTHIW <79294133+AnyTHIW@users.noreply.github.com>
2024-05-07 05:03:30 +0900

    [chore] edit score color

--- src/components/ingame/score.js
+++ src/components/ingame/score.js
@@ -1,13 +1,11 @@
 import React, { useEffect } from 'react';
 
 const Score = ({ hitted, missed }) => {
-  useEffect(() => {
 
-  }, [hitted, missed]);
 
   return (
     <div style={{ position: "absolute", top: "10%", left: "25%", }}>
-      <p style={{ width: "5vw", height: "5vh", fontSize: '24px', border: "1px solid black", color: "black", textAlign: "center" }}>
+      <p style={{ width: "5vw", height: "5vh", fontSize: '24px', border: "1px solid black", color: "green", textAlign: "center" }}>
         {hitted}
       </p>
       <p style={{ width: "5vw", height: "5vh", fontSize: '24px', border: "1px solid black", color: "red", textAlign: "center" }}>
