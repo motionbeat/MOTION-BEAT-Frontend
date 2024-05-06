57e15789cc31c3a897e92fdf1f8cf32c8a8aa628
AnyTHIW <79294133+AnyTHIW@users.noreply.github.com>
2024-05-07 07:19:19 +0900

    [feat] proper judge & update score
    와된다ㅠㅠㅠ

--- src/components/ingame/game/judgement.js
+++ src/components/ingame/game/judgement.js
@@ -1,11 +1,18 @@
 import { Parser } from "../../../utils/parser";
-
-export const Judge = (key, time, judgedNotes) => {
+export const Judge = (key, time) => {
   let result = "ignore"; // 기본 결과를 "ignore"로 설정
+
+  console.log("TEST1");
+  const dispatch = (result) => {
+    const event = new CustomEvent('scoreUpdate', { detail: { result } });
+    window.dispatchEvent(event);
+  };
+
   const notes = document.querySelectorAll('.Note'); // 모든 노트 요소 검색
   let closestNote = null;
   let minIndex = Infinity;
 
+  console.log("TEST2");
   notes.forEach(note => {
     const index = parseInt(note.getAttribute('data-index'), 10); // 요소의 data-index 속성 가져오기
     if (!isNaN(index) && index < minIndex) {  // index가 유효한 숫자인지 확인
@@ -19,18 +26,24 @@ export const Judge = (key, time, judgedNotes) => {
     return result;
   }
 
+  console.log("TEST3");
   const noteTime = parseInt(closestNote.getAttribute('data-time'), 10);
 
-  if (minIndex !== judgedNotes) return result;
+  console.log("MININDEX:" + minIndex + "JUDGEDNOTES:")
   const timeDiff = noteTime - time;
 
-  if (timeDiff > 1000 || closestNote.getAttribute('data-motion') !== Parser(key)) return result;
+  console.log("TEST4");
+  if (timeDiff > 1000 || closestNote.getAttribute('data-motion') !== Parser(key)) {
+    console.log("IGNORE")
+    return dispatch(result);
+  }
+
   if (((timeDiff > 500 && timeDiff < 1000) || (timeDiff < -500 && timeDiff > -1000)) && closestNote.getAttribute('data-motion') === Parser(key)) {
-    result = "miss";
+    console.log("MISS!")
+    dispatch("miss");
   } else if (timeDiff >= -500 && timeDiff <= 500 && closestNote.getAttribute('data-motion') === Parser(key)) {
-    result = "hit";
+    console.log("HIT!")
+    dispatch("hit");
   }
   closestNote.style.display = "none";  // 해당 노트를 화면에서 숨김
-
-  return result; // 모든 검사 후 결과 반환
 }
