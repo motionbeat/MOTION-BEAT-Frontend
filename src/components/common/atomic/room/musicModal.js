9dce93e74283cbde49a6805521352a20cac0a904
SungJunMoon <ansdj3523@gmail.com>
2024-05-06 11:27:20 +0900

    [feat] making music modal UI

--- src/components/common/atomic/room/musicModal.js
+++ src/components/common/atomic/room/musicModal.js
@@ -5,8 +5,18 @@ const MusicModal = () => {
     <>
       <div className="musicModalBox">
         {/* 노래선택 카테고리 */}
-        <div></div>
-        <div></div>
+        <div className="musicModalLeft">
+          <div className="backArrow"></div>
+          <div className="songSelectBox">
+            <div>ALL</div>
+            <div>FAVORITE</div>
+            <div>EASY</div>
+            <div>NORMAL</div>
+            <div>HARD</div>
+          </div>
+        </div>
+        {/* 노래 목록 */}
+        <div className="musicModalRight"></div>
       </div>
     </>
   )
