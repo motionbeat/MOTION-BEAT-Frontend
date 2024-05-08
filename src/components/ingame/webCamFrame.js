08427d4adc11688a984a78c97113445b5020d934
AnyTHIW <79294133+AnyTHIW@users.noreply.github.com>
2024-05-09 01:13:16 +0900

    [fix] edit WebCam wrapper

--- src/components/ingame/webCamFrame.js
+++ src/components/ingame/webCamFrame.js
@@ -6,7 +6,9 @@ const WebCamFrame = ({ children, roomCode, myColor }) => {
   return (
     <div style={{ display: "inline-block" }}>
       <div style={{ border: "10px solid black", position: "relative", display: "block" }}>
-        <Score roomCode={roomCode} />
+        <Score roomCode={roomCode}>
+          {children}
+        </Score>
       </div>
     </div>
   );
