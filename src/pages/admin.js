0138a17bbedc728674d1422c79ea9dbd1b3c8692
SungJunMoon <ansdj3523@gmail.com>
2024-05-08 16:25:05 +0900

    make gameRoom Boom

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const Admin = () => {
   const backendUrl = process.env.REACT_APP_BACK_API_URL;
 
-  const deleteBtn = async () => {
+  const roomBoomBtn = async () => {
     try {
       const response = await axios.delete(`${backendUrl}/api/rooms/admin/delete`, {
           headers: {
@@ -19,9 +19,26 @@ const Admin = () => {
     }
   }
 
+  const gameBoomBtn = async () => {
+    try {
+      const response = await axios.delete(`${backendUrl}/api/games/admin/delete`, {
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
+              "UserId": sessionStorage.getItem("userId"),
+              "Nickname": sessionStorage.getItem("nickname")
+          }
+        });
+
+    } catch (error) {
+        console.error("Error start res:", error);
+    }
+  }
+
   return (
     <>
-      <button style={{position:"absolute", top: "30%", left:"30%" ,width:"300px", height:"300px", fontSize:"50px"}} onClick={deleteBtn}>누르기</button>
+      <button style={{position:"absolute", top: "30%", left:"30%" ,width:"300px", height:"300px", fontSize:"50px"}} onClick={roomBoomBtn}>방 폭파</button>
+      <button style={{position:"absolute", top: "30%", right:"30%" ,width:"300px", height:"300px", fontSize:"50px"}} onClick={gameBoomBtn}>인게임 폭파</button>
     </>
   )
 }
