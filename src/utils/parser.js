207c0975f374b0654f14d795685a54657af5764a
AnyTHIW <79294133+AnyTHIW@users.noreply.github.com>
2024-05-11 03:57:40 +0900

    [feat] feature local keySound & keyEffect
    fix & chore : 데이터 하드코딩, Load에 악기로딩 추가, 판정 범위 변경 및 키이펙트 추가, UI요소 %로 변경, 로그 일부 주석화

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,11 +1,11 @@
 export const Parser = (key) => {
   if (key === "D") {
-    console.log("changeD to A")
+    // console.log("changeD to A")
     return "A"
   }
 
   if (key === "F") {
-    console.log("changeF to B")
+    // console.log("changeF to B")
     return "B"
   }
 
