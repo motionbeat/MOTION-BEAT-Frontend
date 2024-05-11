3427c39b0e5feea91c196ab21077cb0b4d90cd4c
SungJunMoon <ansdj3523@gmail.com>
2024-05-11 11:09:19 +0900

    [feat] making mypage

--- src/components/common/atomic/main/mypageBox.js
+++ src/components/common/atomic/main/mypageBox.js
@@ -1,3 +1,152 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react"
+// import "../../../../styles/main/mypageBox.scss"
+
+// const MypageBox = () => {
+//   const backendUrl = process.env.REACT_APP_BACK_API_URL;
+//   const myNickname = sessionStorage.getItem("nickname");
+//   const [recentSongList, setRecentSongList] = useState([]);
+//   const [favoriteSongList, setFavoriteSongList] = useState([]);
+//   const [activeTab, setActiveTab] = useState('favorite');
+//   // 즐겨찾기 최신 변경
+//   const [changeFav, setChangeFav] = useState(true);
+//   const [changeRec, setChangeRec] = useState(false);
+
+//   const clickFavorite = () => {
+//     setChangeFav(true);
+//     setChangeRec(false);
+    
+//     // // 즐겨찾기 api
+//     // const favoriteSongs = async () => {
+//     //   try {
+//     //     const response = await axios.get(`${backendUrl}/api/songs/favorite`, {
+//     //       headers: {
+//     //         "Content-Type": "application/json",
+//     //         "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
+//     //         "UserId": sessionStorage.getItem("userId"),
+//     //         "Nickname": sessionStorage.getItem("nickname")
+//     //       }
+//     //     });
+//     //     setFavoriteSongList(response.data.recentlyPlayed);
+//     //   } catch (error) {
+//     //     console.error("leave room error", error);
+//     //   }
+//     // };
+
+//     // favoriteSongs();
+//   }
+
+//   // 최근 플레이 api
+//   const clickRecently = () => {
+//     setChangeRec(true);
+//     setChangeFav(false);
+
+//     // const recentlySongs = async () => {
+//     //   try {
+//     //     const response = await axios.get(`${backendUrl}/api/songs/recent`, {
+//     //       headers: {
+//     //         "Content-Type": "application/json",
+//     //         "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
+//     //         "UserId": sessionStorage.getItem("userId"),
+//     //         "Nickname": sessionStorage.getItem("nickname")
+//     //       }
+//     //     });
+//     //     setRecentSongList(response.data);
+//     //   } catch (error) {
+//     //     console.error("leave room error", error);
+//     //   }
+//     // };
+
+//     // recentlySongs();
+//   }
+
+//   useEffect(() => {
+//     if (changeRec || changeFav) {
+//       const fetchData = async () => {
+//         if (changeRec) {
+//           try {
+//             const response = await axios.get(`${backendUrl}/api/songs/recent`, {
+//               headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
+//                 "UserId": sessionStorage.getItem("userId"),
+//                 "Nickname": sessionStorage.getItem("nickname")
+//               }
+//             });
+//             setRecentSongList(response.data.recentlyPlayed);
+//           } catch (error) {
+//             console.error("leave room error", error);
+//           }
+//         } else if (changeFav) {
+//           try {
+//             const response = await axios.get(`${backendUrl}/api/songs/favorite`, {
+//               headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
+//                 "UserId": sessionStorage.getItem("userId"),
+//                 "Nickname": sessionStorage.getItem("nickname")
+//               }
+//             });
+//             setFavoriteSongList(response.data);
+//           } catch (error) {
+//             console.error("leave room error", error);
+//           }
+//         }
+//       };
+//       fetchData();
+//     }
+//   }, [changeRec, changeFav]);
+
+//   return (
+//     <>
+//       <div className="mypageBoxWrapper">
+//         <div className="mypageNickname">
+//           <p>{myNickname}</p>
+//         </div>
+
+//         <div className="mypageMainBox">
+//           <div className="mypageFavorite" onClick={clickFavorite} style={{ zIndex: changeFav ? 3 : 2 }}>
+//             {changeFav &&
+//               <>
+//                 <span className="favoriteTitle">즐겨찾기 한 노래</span>
+//                 <ul style={{fontSize: "2rem", color: "white"}}>
+//                   {favoriteSongList.map((song, index) => (
+//                     <li key={index}>
+//                       <p>{song.title}</p>
+//                       <p>{song.artist}</p>
+//                       <p>{song.difficulty}</p>
+//                       </li>
+//                   ))}
+//                 </ul>
+//               </>
+//             }
+//           </div>
+          
+//           <div className="mypageRecently" onClick={clickRecently} style={{ zIndex: changeRec ? 3 : 2 }}>
+//             {changeRec &&
+//               <>
+//                 <span className="recentlyTitle">최근 플레이 한 노래</span>
+//                 <ul style={{fontSize: "2rem", color: "white"}}>
+//                   {recentSongList.map((song, index) => (
+//                     <li key={index}>
+//                       <p>{song.title}</p>
+//                       <p>{song.artist}</p>
+//                       <p>{song.difficulty}</p>
+//                       </li>
+//                   ))}
+//                 </ul>
+//               </>
+//             }
+//           </div>
+//         </div>
+
+
+//       </div>
+//     </>
+//   )
+// }
+// export default MypageBox
+
 import axios from "axios";
 import { useEffect, useState } from "react"
 import "../../../../styles/main/mypageBox.scss"
@@ -7,94 +156,40 @@ const MypageBox = () => {
   const myNickname = sessionStorage.getItem("nickname");
   const [recentSongList, setRecentSongList] = useState([]);
   const [favoriteSongList, setFavoriteSongList] = useState([]);
-  // 즐겨찾기 최신 변경
-  const [changeFav, setChangeFav] = useState(true);
-  const [changeRec, setChangeRec] = useState(false);
+  const [activeTab, setActiveTab] = useState('favorite'); // 상태 추가
 
-  const clickFavorite = () => {
-    setChangeFav(true);
-    setChangeRec(false);
-    
-    // // 즐겨찾기 api
-    // const favoriteSongs = async () => {
-    //   try {
-    //     const response = await axios.get(`${backendUrl}/api/songs/favorite`, {
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //         "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
-    //         "UserId": sessionStorage.getItem("userId"),
-    //         "Nickname": sessionStorage.getItem("nickname")
-    //       }
-    //     });
-    //     setFavoriteSongList(response.data.recentlyPlayed);
-    //   } catch (error) {
-    //     console.error("leave room error", error);
-    //   }
-    // };
+  const fetchData = async () => { // 데이터 가져오는 함수
+    try {
+      const response = await axios.get(`${backendUrl}/api/songs/${activeTab}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
+          "UserId": sessionStorage.getItem("userId"),
+          "Nickname": sessionStorage.getItem("nickname")
+        }
+      });
 
-    // favoriteSongs();
-  }
-
-  // 최근 플레이 api
-  const clickRecently = () => {
-    setChangeRec(true);
-    setChangeFav(false);
-
-    // const recentlySongs = async () => {
-    //   try {
-    //     const response = await axios.get(`${backendUrl}/api/songs/recent`, {
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //         "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
-    //         "UserId": sessionStorage.getItem("userId"),
-    //         "Nickname": sessionStorage.getItem("nickname")
-    //       }
-    //     });
-    //     setRecentSongList(response.data);
-    //   } catch (error) {
-    //     console.error("leave room error", error);
-    //   }
-    // };
-
-    // recentlySongs();
-  }
+      if (activeTab === 'favorite') {
+        setFavoriteSongList(response.data);
+        console.log(response.data);
+      } else if (activeTab === 'recent') {
+        setRecentSongList(response.data.recentlyPlayed);
+        console.log(response.data.recentlyPlayed);
+      }
+    } catch (error) {
+      console.error("leave room error", error);
+    }
+  };
 
   useEffect(() => {
-    if (changeRec || changeFav) {
-      const fetchData = async () => {
-        if (changeRec) {
-          try {
-            const response = await axios.get(`${backendUrl}/api/songs/recent`, {
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
-                "UserId": sessionStorage.getItem("userId"),
-                "Nickname": sessionStorage.getItem("nickname")
-              }
-            });
-            setRecentSongList(response.data);
-          } catch (error) {
-            console.error("leave room error", error);
-          }
-        } else if (changeFav) {
-          try {
-            const response = await axios.get(`${backendUrl}/api/songs/favorite`, {
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${sessionStorage.getItem("userToken")}`,
-                "UserId": sessionStorage.getItem("userId"),
-                "Nickname": sessionStorage.getItem("nickname")
-              }
-            });
-            setFavoriteSongList(response.data);
-          } catch (error) {
-            console.error("leave room error", error);
-          }
-        }
-      };
-      fetchData();
-    }
-  }, [changeRec, changeFav]);
+    fetchData();
+    console.log("Active tab is now:", activeTab);
+  }, [activeTab]); // activeTab이 변경될 때마다 fetchData 호출
+
+  const handleClick = (tab) => { // 탭 클릭 시 호출되는 함수
+    console.log("Changing tab to:", tab);
+    setActiveTab(tab);
+  };
 
   return (
     <>
@@ -104,44 +199,39 @@ const MypageBox = () => {
         </div>
 
         <div className="mypageMainBox">
-          <div className="mypageFavorite" onClick={clickFavorite} style={{ zIndex: changeFav ? 3 : 2 }}>
-            {changeFav &&
-              <>
-                <span className="favoriteTitle">즐겨찾기 한 노래</span>
-                <ul style={{fontSize: "2rem", color: "white"}}>
-                  {favoriteSongList.map((song, index) => (
-                    <li key={index}>
-                      <p>{song.title}</p>
-                      <p>{song.artist}</p>
-                      <p>{song.difficulty}</p>
-                      </li>
-                  ))}
-                </ul>
-              </>
+          <div className="mypageFavorite" onClick={() => handleClick('favorite')} style={{ zIndex: activeTab === 'favorite' ? 3 : 2 }}>
+            <span className="favoriteTitle">즐겨찾기 한 노래</span>
+            {activeTab === 'favorite' && favoriteSongList && (favoriteSongList.length > 0) &&
+              <ul style={{fontSize: "2rem", color: "white"}}>
+                {favoriteSongList.map((song, index) => (
+                  <li key={index}>
+                    <p>{song.title}</p>
+                    <p>{song.artist}</p>
+                    <p>{song.difficulty}</p>
+                  </li>
+                ))}
+              </ul>
             }
           </div>
           
-          <div className="mypageRecently" onClick={clickRecently} style={{ zIndex: changeRec ? 3 : 2 }}>
-            {changeRec &&
-              <>
-                <span className="recentlyTitle">최근 플레이 한 노래</span>
-                <ul style={{fontSize: "2rem", color: "white"}}>
-                  {recentSongList.map((song, index) => (
-                    <li key={index}>
-                      <p>{song.title}</p>
-                      <p>{song.artist}</p>
-                      <p>{song.difficulty}</p>
-                      </li>
-                  ))}
-                </ul>
-              </>
+          <div className="mypageRecently" onClick={() => handleClick('recent')} style={{ zIndex: activeTab === 'recent' ? 3 : 2 }}>
+            <span className="recentlyTitle">최근 플레이 한 노래</span>
+            {activeTab === 'recent' && recentSongList && (recentSongList.length > 0) &&
+              <ul style={{fontSize: "2rem", color: "white"}}>
+                {recentSongList.map((song, index) => (
+                  <li key={index}>
+                    <p>{song.title}</p>
+                    <p>{song.artist}</p>
+                    <p>{song.difficulty}</p>
+                  </li>
+                ))}
+              </ul>
             }
           </div>
         </div>
-
-
       </div>
     </>
   )
 }
-export default MypageBox
+
+export default MypageBox;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,10 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { CheckLoginValidate } from "../utils/checkValidate";
 import { KakaoLoginButton } from "../apis/kko";
 import { GoogleLoginButton } from "../apis/ggl";
 import socket from "../server/server.js";
-import styled from "styled-components";
 import "../styles/login.scss";
 import {login} from "../server/socketEvents.js"
 import BackArrow from "../img/backArrow.png"
@@ -90,56 +89,74 @@ const Login = () => {
     navigate("/signup");
   };
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [
+    "../img/loginpage0",
+    "../img/loginpage1"
+  ];
+
+  const png = ".png";
+
+
+  const changeBackgroundImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(changeBackgroundImage, 3000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const currentImagePath = images[currentIndex] + png;
+
   return (
     <>
-      {/* <h1 className="login-title">MOTION BEAT</h1> */}
-      <div className="loginWrapper">
-        <div className="loginForm">
-          <div className="loginHeader">
-            <div className="loginBackArrow"><img src={BackArrow} alt="뒤로가기" /></div>
-            <div className="loginTitle">로그인</div>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className="formInputWrapper">
-              <p>ID</p>
-              <div className="inputBox">
-                <img src={emailIcon} alt="이메일아이콘" />
-                <input type="text" placeholder="motion@gmail.com" ref={emailRef} />
+      <div className="bg" style={{ backgroundImage: `url(${currentImagePath})`}}>
+        {/* <h1 className="login-title">MOTION BEAT</h1> */}
+        <div className="loginWrapper">
+          <div className="loginForm">
+            <div className="loginHeader">
+              <div className="loginBackArrow"><img src={BackArrow} alt="뒤로가기" /></div>
+              <div className="loginTitle">로그인</div>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="formInputWrapper">
+                <p>ID</p>
+                <div className="inputBox">
+                  <img src={emailIcon} alt="이메일아이콘" />
+                  <input type="text" placeholder="motion@gmail.com" ref={emailRef} />
+                </div>
+                {errors.email && <p style={{ color: 'red' }}>{errors.email[0]}</p>}
+              </div >
+              <div className="formInputWrapper">
+                <p>Password</p>
+                <div className="inputBox">
+                  <img src={pwIcon} alt="비번아이콘" />
+                  <input type="password" placeholder="********" ref={pwRef} />
+                </div>
+                {errors.pw && <p style={{ color: 'red' }}>{errors.pw[0]}</p>}
               </div>
-              {errors.email && <p style={{ color: 'red' }}>{errors.email[0]}</p>}
-            </div >
-            <div className="formInputWrapper">
-              <p>Password</p>
-              <div className="inputBox">
-                <img src={pwIcon} alt="비번아이콘" />
-                <input type="password" placeholder="********" ref={pwRef} />
+              <div className="subFuncBox">
+                <div>
+                  <input type="checkbox" />
+                  <p>아이디 저장</p>
+                </div>
+                <p onClick={handleForgot}>비밀번호 찾기</p>
               </div>
-              {errors.pw && <p style={{ color: 'red' }}>{errors.pw[0]}</p>}
-            </div>
-            <div className="subFuncBox">
-              <div>
-                <input type="checkbox" />
-                <p>아이디 저장</p>
+              <div className="loginBtnBox">
+                <button type="submit">로그인</button>
               </div>
-              <p onClick={handleForgot}>비밀번호 찾기</p>
-            </div>
-            <div className="loginBtnBox">
-              <button type="submit">로그인</button>
-            </div>
-            <div className="socialLogin">
-              <KakaoLoginButton setEvent={setPopupClosedByUser}>Login With Kakao</KakaoLoginButton>
-              {/* <div className="dummy"></div> */}
-              <GoogleLoginButton setEvent={setPopupClosedByUser}>Login with Google</GoogleLoginButton>
-            </div>
-              {/* {popupClosedByUser && <p>로그인 창이 닫혔습니다. 다시 시도해 주세요.</p>} */}
-              <div className="signupBox">
-                <p onClick={handleSignup}>아직 회원이 아니신가요?<span>회원가입</span></p>
+              <div className="socialLogin">
+                <KakaoLoginButton setEvent={setPopupClosedByUser}>Login With Kakao</KakaoLoginButton>
+                {/* <div className="dummy"></div> */}
+                <GoogleLoginButton setEvent={setPopupClosedByUser}>Login with Google</GoogleLoginButton>
               </div>
-          </form>
-          <div>
-            <div>
-              
-            </div>
+                {/* {popupClosedByUser && <p>로그인 창이 닫혔습니다. 다시 시도해 주세요.</p>} */}
+                <div className="signupBox">
+                  <p onClick={handleSignup}>아직 회원이 아니신가요?<span>회원가입</span></p>
+                </div>
+            </form>
           </div>
         </div>
       </div>
