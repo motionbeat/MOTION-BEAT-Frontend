31c0a6029ed16995f78a70ad3d4e62d29a0d8b4e
Kim Hyeonwoo <1reds@naver.com>
2024-05-11 20:09:16 +0900

    [fix] Remove openvidu console log

--- src/components/room/webCam.js
+++ src/components/room/webCam.js
@@ -252,6 +252,8 @@ const WebCam = ({ players = [], hostName, roomCode, ingame }) => {
             window.addEventListener("keydown", (event) => {
               const key = event.key.toUpperCase();
 
+              // console.log("[KHW] Key pressed: " + key); // Message
+
               // 키 입력 시, 해당 키를 다른 사용자에게 전달
               // 현재 키 입력은 D, F만 전달 (참고: import { Parser } from "../../../utils/parser")
               if (["D", "F"].includes(key)) {
