991e3b9a8b51395bd0eb1bfaa2ff4696d1545793
SungJunMoon <ansdj3523@gmail.com>
2024-05-13 22:43:39 +0900

    [fix] webframe and score visibility hidden

--- src/components/ingame/webCamFrame.js
+++ src/components/ingame/webCamFrame.js
@@ -4,7 +4,7 @@ import Score from "./score";
 const WebCamFrame = ({ children, roomCode, myColor }) => {
 
   return (
-    <div style={{ display: "inline-block" }}>
+    <div style={{ display: "inline-block" , visibility:"hidden"}}>
       <div style={{ border: `10px solid ${myColor}`, position: "relative", display: "block" }}>
         <JudgeEffect />
         <JudgeEffectV2 />
